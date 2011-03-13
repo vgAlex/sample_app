f4d16c01872d536cc7343e8f34947be228c45604
vgAlex <alex.virtuogenesis@gmail.com>
2011-03-12 16:10:01 -0800

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,19 @@
 SampleApp::Application.routes.draw do
-  
+
+  get "users/new"
   
   root :to => 'pages#home'
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
   
+  match '/signup', :to => 'users#new'
   
   
   #get "pages/home"
   #get "pages/contact"
   #get "pages/about"
-    
+ 
     
 
   # The priority is based upon order of creation:
