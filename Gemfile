eaf868251731ced027820d710477c43f57e503ed
vgAlex <alex.virtuogenesis@gmail.com>
2011-03-16 23:24:41 -0700

    User model with passwords, and user show page

--- Gemfile
+++ Gemfile
@@ -34,11 +34,14 @@ group :development do
 	gem 'rspec-rails', '2.0.1'
 	#gem 'annotate-models', '1.0.4'
 	gem 'annotate'
+	gem 'gravatar_image_tag', '0.1.0'
 end
 
 group :test do
 	gem 'rspec', '2.0.1'
 	gem 'webrat', '0.7.1'
+	gem 'factory_girl_rails', '1.0'
+	gem 'gravatar_image_tag', '0.1.0'
 end
 
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,29 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+
+
+
 require 'spec_helper'
 
 describe User do
   #pending "add some examples to (or delete) #{__FILE__}"
   
   before(:each) do
-    @attr = { :name => "Example User", :email => "user@example.com"}
+    @attr = { 
+              :name => "Example User", 
+              :email => "user@example.com",
+              :password => "foobar",
+              :password_confirmation => "foobar"
+            }
   end
   
   it "should create a new instance given valid attributes" do
@@ -73,20 +92,105 @@ describe User do
   
   
   
+
+  
+  
+  
+  #|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+  
+  
+  
+  describe "password encryption" do
+    
+    before(:each) do
+      @user = User.create!(@attr)
+    end
+    
+    
+    # Password -- Chapter 7.1 -----------------------------------------------------
+    it "should require a password" do
+      User.new(@attr.merge(:password => "", :password_confirmation => "")).
+        should_not be_valid
+    end
+
+    it "should require a matching password confirmation" do
+      User.new(@attr.merge(:password_confirmation => "invalid")).
+        should_not be_valid
+    end
+
+    it "should reject short passwords" do
+      short = "a" * 5
+      hash = @attr.merge(:password => short, :password_confirmation => short)
+      User.new(hash).should_not be_valid
+    end
+
+    it "should reject long passwords" do
+      long = "a" * 41
+      hash = @attr.merge(:password => long, :password_confirmation => long)
+      User.new(hash).should_not be_valid
+    end
+
+    it "should have an ecrypted password attribute" do
+      @user.should respond_to(:encrypted_password)
+    end
+
+    it "should set the encrypted password" do
+      @user.encrypted_password.should_not be_blank
+    end    
+    # Password -- Chapter 7.1 -----------------------------------------------------
+    
+    
+    
+    
+    # Password -- Chapter 7.2 -----------------------------------------------------
+    describe "has_password? method" do
+      
+      it "should be true if the password match" do
+        @user.has_password?(@attr[:password]).should be_true
+      end
+      
+      it "should be false if the passowrds don't match" do
+        @user.has_password?("invalid").should be_false
+      end
+
+    end
+    
+    
+    
+    
+    describe "password encryption" do
+    
+      it "should return nil on email/password mismatch" do
+        wrong_password_user = User.authenticate(@attr[:email], "wrongpass")
+        wrong_password_user.should be_nil
+      end
+      
+      it "should return nil for an email address with no user" do
+        nonexistent_user = User.authenticate("bar@foo.com", @attr[:password])
+        nonexistent_user.should be_nil
+      end
+      
+      it "should return the user on email/password match" do
+        matching_user = User.authenticate(@attr[:email], @attr[:password])
+        matching_user.should == @user
+      end
+      
+    end
+    
+    # Password -- Chapter 7.2 -----------------------------------------------------
+    
+    
+    
+    
+    
+    
+  end
+  
   
   
   
   
 end
 
-# == Schema Information
-#
-# Table name: users
-#
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
-#
+
 
